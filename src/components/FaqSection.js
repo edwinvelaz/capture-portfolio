ac4561d3a910264ac011e22e08cba1ac6696bb42
Edwin Velazquez <itslyoku@gmail.com>
2021-03-08 13:40:54 -0600

    Styled FaqSection

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import styled from 'styled-components';
+import { StyledAbout } from '../styles';
 
 const FaqSection = () => {
     return (
-        <div className="faq">
+        <StyledFaq>
             <h2>Any Questions <span>FAQ</span></h2>
             <div className="question">
                 <h4>How Do I Start</h4>
@@ -10,6 +12,7 @@ const FaqSection = () => {
                     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
                     <p>Asperiores dicta molestias id tempore aliquam.</p>
                 </div>
+                <div className="faq-line"></div>
             </div>
             <div className="question">
                 <h4>Daily Schedule</h4>
@@ -17,6 +20,7 @@ const FaqSection = () => {
                     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
                     <p>Asperiores dicta molestias id tempore aliquam.</p>
                 </div>
+                <div className="faq-line"></div>
             </div>
             <div className="question">
                 <h4>Different Payment Methods</h4>
@@ -24,6 +28,7 @@ const FaqSection = () => {
                     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
                     <p>Asperiores dicta molestias id tempore aliquam.</p>
                 </div>
+                <div className="faq-line"></div>
             </div>
             <div className="question">
                 <h4>What Product do you offer</h4>
@@ -31,9 +36,38 @@ const FaqSection = () => {
                     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
                     <p>Asperiores dicta molestias id tempore aliquam.</p>
                 </div>
+                <div className="faq-line"></div>
             </div>
-        </div>
+        </StyledFaq>
     )
 }
+
+const StyledFaq = styled(StyledAbout)`
+    display: block;
+    padding-left: 15rem;
+    span{
+        display: block;
+    }
+    h2{
+        padding-bottom: 2rem;
+        font-weight: lighter;
+    }
+    .faq-line {
+        background: #cccccc;
+        height: 0.2rem;
+        margin: 2rem 0rem;
+        width: 100%;
+    }
+    .question {
+        padding: 3rem 0rem;
+        cursor: pointer;
+    }
+    .answer {
+        padding: 2rem 0rem;
+        p{
+            padding: 1rem 0rem;
+        }
+    }
+`
 
 export default FaqSection;
